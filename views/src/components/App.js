3cbe8b8344c8cc49f93d2e8295ad30875e50195d
thomascrumrine <tommy@apruve.com>
2017-06-12 13:11:20 -0500

    AP-5299: Removed ugly navbar for now

--- views/src/components/App.js
+++ views/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import { Container } from 'reactstrap'
 import OrderForm from './OrderForm'
 
 class App extends React.Component {
@@ -7,19 +7,6 @@ class App extends React.Component {
     return (
       <div>
         <Container>
-          <Row>
-            <Navbar color="faded" light>
-              <NavbarBrand href="/">apruve</NavbarBrand>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="/">Orders</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/orders">Offline Orders</NavLink>
-                </NavItem>
-              </Nav>
-            </Navbar>
-          </Row>
           <OrderForm />
         </Container>
       </div>
