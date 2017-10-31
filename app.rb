4053f943b61266fa28d7fd3168a8a2be94938c52
Dan Belling <danhbelling@gmail.com>
2017-10-31 09:42:00 -0500

    AP-6243 Updated OrderItems in app

--- app.rb
+++ app.rb
@@ -110,17 +110,17 @@ get '/' do
       sku:              'LTR-20R',
       price_ea_cents:   1200,
       quantity:         3,
-      amount_cents:     3600,
+      price_total_cents:     3600,
       view_product_url: 'https://merchant-demo.herokuapp.com'
   )
   @order.order_items << Apruve::OrderItem.new(
-      title:            'Legal Paper',
-      description:      '24 lb ream (250 Sheets). Paper dimensions are 8.5 x 14.00 inches.',
-      sku:              'LGL-24R',
-      price_ea_cents:   950,
-      quantity:         2,
-      amount_cents:     1900,
-      view_product_url: 'https://merchant-demo.herokuapp.com'
+      title:             'Legal Paper',
+      description:       '24 lb ream (250 Sheets). Paper dimensions are 8.5 x 14.00 inches.',
+      sku:               'LGL-24R',
+      price_ea_cents:    950,
+      quantity:          2,
+      price_total_cents: 1900,
+      view_product_url:  'https://merchant-demo.herokuapp.com'
   )
   erb :index
 end
